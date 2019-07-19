6e2ba5653ef14d3667751f209e0fd48cb8c63a32
Rohit095 <ringmasterrohit@gmail.com>
2019-07-19 12:29:19 +0530

    look

---
+++ Hello/src/mine/Helloworld2.java
@@ -0,0 +1,11 @@
+package mine;
+
+public class Helloworld2 {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+		System.out.println("its all about to add new culture");
+
+	}
+
+}
